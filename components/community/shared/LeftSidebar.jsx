5ce932d665d3c308a3112105cc466a09262a5272
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-02-09 11:28:21 +0530

    ADD:right bar done

--- components/community/shared/LeftSidebar.jsx
+++ components/community/shared/LeftSidebar.jsx
@@ -12,10 +12,7 @@ const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   return (
-    <section
-      style={{ backgroundColor: "black", color: "white" }}
-      className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-28 max-md:hidden"
-    >
+    <section className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-28 max-md:hidden">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
           const isActive =
